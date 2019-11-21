f55a5450a6a2c9d82744be9b61714553769dbf47
ahaigh9877 <alastairhaigh@me.com>
2019-11-21 17:47:18 +0100

    Css

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -8,29 +8,44 @@ import questionMark from "../../images/question.png";
 class Game extends Component {
   render() {
     const {
-      match: { params: { name } },
+      match: {
+        params: { name }
+      },
       loggedin: { username },
       rooms
-    } = this.props
+    } = this.props;
     // const name = this.props.match.params.name
     // const username = this.props.loggedin.username
 
-    const room = rooms.find(room => room.name === name)
-    const user = room.users.find(user => user.username === username)
-    const index = room.users.findIndex(user => user.username === username)
-    const other = room.users.find(user => user.username !== username)
+    const room = rooms.find(room => room.name === name);
+    const user = room.users.find(user => user.username === username);
+    const index = room.users.findIndex(user => user.username === username);
+    const other = room.users.find(user => user.username !== username);
 
-    const [first, second] = room.users
+    const [first, second] = room.users;
     // const first = room.users[0]
     // const second = room.user[1]
 
-    const imFirst = first === user
+    const imFirst = first === user;
+    // const imOther = first !== user
 
     return (
       <div id="gameWrapper">
-        <div id="player1Score">{first.username}'s score: {first.score}</div>
-        <div id="player2Score">{second.username}'s score: {second.score}</div>
-        
+        <div id="player1Score">
+          {first.username}'s
+          <br />
+          score:
+          <br />
+          <p className="scoreNr">{first.score}</p>
+        </div>
+        <div id="player2Score">
+          {second.username}'s
+          <br />
+          score:
+          <br />
+          <p className="scoreNr">{second.score}</p>
+        </div>
+
         <div id="player1ChoiceImageWrapper">
           <img className="questionMark" src={questionMark} alt="choice 1" />
         </div>
@@ -47,14 +62,13 @@ class Game extends Component {
           <button className="selectButton" onClick={() => this.props.choose(2)}>
             <img className="choiceImg" src={rock} alt="choice 2" />
           </button>
-          <button
-            className="selectButton"
-            onClick={() => this.props.choose(3)}
-          >
+          <button className="selectButton" onClick={() => this.props.choose(3)}>
             <img className="choiceImg" src={scissors} alt="choice 3" />
           </button>
         </div>
-        <button id="chooseButton" onClick={() => this.props.setChoice()}>CHOOSE!</button>
+        <button id="chooseButton" onClick={() => this.props.setChoice()}>
+          CHOOSE!
+        </button>
       </div>
     );
   }
